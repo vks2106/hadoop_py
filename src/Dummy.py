79ed57cc77b8f85700fc184af552cbc767cfa833
vinod <vinod-kumar.a.sharma@capgemini.com>
2019-07-15 16:11:00 +0200

    added a dummy class

---
+++ src/Dummy.py
@@ -0,0 +1,18 @@
+class Employee:
+    'Common base class for all employees'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+   
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print ("Name : ", self.name, ", Salary: ", self.salary)
+
+if __name__ == '__main__':
+    emp = Employee("vinod",50)
+    emp.displayEmployee()
